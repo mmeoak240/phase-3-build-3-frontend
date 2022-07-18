8c23eddd34aa229504bf5dc37bea32989ff57a3d
Michael Meoak <mmeoak240@gmail.com>
2022-07-18 16:44:01 -0700

    added BrowserRouter

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,7 +4,7 @@ const NavBar = () => {
 	return (
 		<div>
 			<nav style={{ textAlign: "center", background: "#49fb35" }}>
-				<NavLink style={{ marginRight: "30px", fontSize: "20px" }} to="/">
+				<NavLink style={{ marginRight: "30px", fontSize: "20px" }} to="#">
 					Home
 				</NavLink>
 			</nav>
